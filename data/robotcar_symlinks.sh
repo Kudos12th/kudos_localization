8eeca70212bf97971d687dd82f0635f620ce2421
YunHaaaa <qlxqlrt2012@gmail.com>
2024-02-02 01:01:41 +0900

    Feat: robotcar data

--- data/robotcar_symlinks.sh
+++ data/robotcar_symlinks.sh
@@ -2,8 +2,8 @@
 
 set -x
 # change the directory
-ROBOTCAR_SDK_ROOT=/root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar-dataset-sdk
+ROBOTCAR_SDK_ROOT=~/dir/AtLoc/data/robotcar-dataset-sdk
 
-ln -s ${ROBOTCAR_SDK_ROOT}/models/ /root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar_camera_models
-ln -s ${ROBOTCAR_SDK_ROOT}/python/ /root/workspace/docker_workspace/workspace/py36pt10/Loc/AtLoc/data/robotcar_sdk
+ln -s ${ROBOTCAR_SDK_ROOT}/models/ ~/dir/AtLoc/data/robotcar_camera_models
+ln -s ${ROBOTCAR_SDK_ROOT}/python/ ~/dir/AtLoc/data/robotcar_sdk
 set +x
