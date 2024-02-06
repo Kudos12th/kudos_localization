a119357fd1c6ec4df974eeb3d1f1922ba677942d
YunHaaaa <qlxqlrt2012@gmail.com>
2024-02-06 19:24:35 +0900

    Feat: fix topic

--- data/Kudos/labeling.py
+++ data/Kudos/labeling.py
@@ -1,8 +1,7 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import Point, Quaternion
-from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import cv2
 import os
 
@@ -10,8 +9,8 @@ class ImageSubscriberNode:
     def __init__(self):
         rospy.init_node('image_subscriber_node', anonymous=True)
 
-        # 이미지 구독자 초기화
-        rospy.Subscriber('image_coordinates_quaternion', PointQuaternion, self.image_callback)
+        # AMCL 토픽에서 로봇의 위치 메시지를 구독합니다.
+        rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, self.amcl_pose_callback)
 
         # OpenCV 초기화
         self.bridge = CvBridge()
@@ -23,23 +22,27 @@ class ImageSubscriberNode:
         self.output_folder = 'received_images'
         os.makedirs(self.output_folder, exist_ok=True)
 
-    def image_callback(self, image_info):
+    def amcl_pose_callback(self, amcl_pose_msg):
         try:
             # 프레임 읽기
             ret, frame = self.cap.read()
 
-            # 쿼터니언을 오일러 앵글로 변환
-            quaternion = image_info.quaternion
-            euler_angles = euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
+            # 로봇의 위치 메시지에서 x, y 좌표 및 orientation의 x, y, z, w 값을 추출
+            x = amcl_pose_msg.pose.pose.position.x
+            y = amcl_pose_msg.pose.pose.position.y
+            orientation_x = amcl_pose_msg.pose.pose.orientation.x
+            orientation_y = amcl_pose_msg.pose.pose.orientation.y
+            orientation_z = amcl_pose_msg.pose.pose.orientation.z
+            orientation_w = amcl_pose_msg.pose.pose.orientation.w
 
-            # 받은 좌표와 오일러 앵글로 이미지 저장
-            image_filename = f"{self.output_folder}/received_image_{int(image_info.coordinates.x)}_{int(image_info.coordinates.y)}_"
-            image_filename += f"Roll_{int(euler_angles[0])}_Pitch_{int(euler_angles[1])}_Yaw_{int(euler_angles[2])}.jpg"
+            # 받은 좌표와 orientation을 파일 이름으로 사용하여 이미지 저장
+            image_filename = f"{self.output_folder}/received_image_x_{int(x)}_y_{int(y)}_"
+            image_filename += f"orient_x_{int(orientation_x)}_orient_y_{int(orientation_y)}_orient_z_{int(orientation_z)}_orient_w_{int(orientation_w)}.jpg"
             
             cv2.imwrite(image_filename, frame)
 
-            rospy.loginfo(f"Received coordinates: ({image_info.coordinates.x}, {image_info.coordinates.y}), "
-                          f"Quaterion: {quaternion}, "
+            rospy.loginfo(f"Received AMCL pose: ({x}, {y}), "
+                          f"Orientation: ({orientation_x}, {orientation_y}, {orientation_z}, {orientation_w}), "
                           f"Saved image: {image_filename}")
 
         except CvBridgeError as e:
