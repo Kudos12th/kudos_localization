999f9c4216e9222ae7b5115373f5e90c490dacc9
annaayeon <ayeon52@kookmin.ac.kr>
2024-02-08 20:42:55 +0900

    Feat: add dxl angle_callback and euler

--- data/Kudos/labeling.py
+++ data/Kudos/labeling.py
@@ -3,6 +3,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import Odometry 
+from std_msgs.msg import String
+import math
 import cv2
 import os
 
@@ -11,8 +13,8 @@ class ImageSubscriberNode:
         rospy.init_node('image_subscriber_node', anonymous=True)
 
         # AMCL 토픽에서 로봇의 위치 메시지를 구독합니다.
-        rospy.Subscriber('/move_base_simple/goal', Pose, self.move_base_goal_callback)
         rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        rospy.Subscriber('/dxl_ang',String , self.angle_callback)
 
         # OpenCV 초기화
         self.bridge = CvBridge()
@@ -25,39 +27,63 @@ class ImageSubscriberNode:
         os.makedirs(self.output_folder, exist_ok=True)
 
         # 콜백에서 받은 값 저장을 위한 변수
-        self.move_base_x = 0.0
-        self.move_base_y = 0.0
         self.odom_x = 0.0
         self.odom_y = 0.0
         self.odom_z = 0.0
-        self.odom_w = 0.0
-
-    def move_base_goal_callback(self, move_base_goal_msg):
-        # MoveBase의 목표 위치 메시지에서 x, y 좌표를 추출하여 변수에 저장
-        self.move_base_x = move_base_goal_msg.position.x
-        self.move_base_y = move_base_goal_msg.position.y
+        self.ori_x = 0.0
+        self.ori_y = 0.0
+        self.ori_z = 0.0
+        self.ori_w = 0.0
+        self.yaw = 0.0
+        self.angle = 0.0
+        self.n = 0
 
     def odom_callback(self, odom_msg):
         # Odometry 메시지에서 x, y, z, w 좌표를 추출하여 변수에 저장
         self.odom_x = odom_msg.pose.pose.position.x
         self.odom_y = odom_msg.pose.pose.position.y
-        self.odom_z = odom_msg.pose.pose.orientation.z
-        self.odom_w = odom_msg.pose.pose.orientation.w
+        self.odom_z = odom_msg.pose.pose.position.z
+        
+        self.ori_x = odom_msg.pose.pose.orientation.x
+        self.ori_y = odom_msg.pose.pose.orientation.y
+        self.ori_z = odom_msg.pose.pose.orientation.z
+        self.ori_w = odom_msg.pose.pose.orientation.w
+        self.yaw = euler_from_quaternion(self.ori_x,self.ori_y,self.ori_z,self.ori_w)
+
+    def angle_callback(self, angle_msg):
+        self.angle = angle_msg.data
+        image_subscriber_node.save_image()
+        print(self.angle)
+ 
+    def euler_from_quaternion(self, x, y, z, w):
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return yaw_z # in radians
 
     def save_image(self):
         try:
             # 프레임 읽기
+            self.n+=1
             ret, frame = self.cap.read()
 
             # 받은 좌표를 파일 이름으로 사용하여 이미지 저장
-            image_filename = f"{self.output_folder}/{self.move_base_x}_{self.move_base_y}_"
-            image_filename += f"{self.odom_x}_{self.odom_y}_{self.odom_z}_{self.odom_w}.jpg"
+            image_filename = f"{self.output_folder}/{self.n}_"
+            image_filename += f"{self.odom_x}_{self.odom_y}_{self.yaw}_{self.angle}.jpg"
 
             cv2.imwrite(image_filename, frame)
 
-            rospy.loginfo(f"Received MoveBase goal: ({self.move_base_x}, {self.move_base_y}), "
-                          f"Odometry data: ({self.odom_x}, {self.odom_y}, {self.odom_z}, {self.odom_w}), "
-                          f"Saved image: {image_filename}")
+            rospy.loginfo(f"Saved image: {image_filename}")
 
         except CvBridgeError as e:
             rospy.logerr(e)
@@ -67,10 +93,10 @@ if __name__ == "__main__":
         image_subscriber_node = ImageSubscriberNode()
         
         # rospy.Rate를 사용하여 이미지 저장 주기 설정 (예: 1초에 한 번)
-        rate = rospy.Rate(1)
+        rate = rospy.Rate(50)
 
         while not rospy.is_shutdown():
-            image_subscriber_node.save_image()
+            #image_subscriber_node.save_image()
             rate.sleep()
 
     except rospy.ROSInterruptException:
