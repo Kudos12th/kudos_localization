8fe06013a274f48323ab6dd16e2a885eb1d72f3b
YunHaaaa <qlxqlrt2012@gmail.com>
2024-02-13 19:10:20 +0900

    Fix: load image

--- detect.py
+++ detect.py
@@ -55,13 +55,56 @@ class priROS:
         self.angle_pub.publish(msg)
 
 
+
+
+def resize_and_pad(image, desired_size):
+    old_size = image.shape[:2] 
+    ratio = float(desired_size/max(old_size))
+    new_size = tuple([int(x*ratio) for x in old_size])
+    
+    # new_size should be in (width, height) format
+    
+    image = cv2.resize(image, (new_size[1], new_size[0]))
+    
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    
+    pad = (delta_w, delta_h)
+    
+    color = [100, 100, 100]
+    new_im = cv2.copyMakeBorder(image, 0, delta_h, 0, delta_w, cv2.BORDER_CONSTANT,
+        value=color)
+        
+    return new_im, pad
+
+def get_image_tensor(img, max_size, debug=False):
+    """
+    Reshapes an input image into a square with sides max_size
+    """
+    if type(img) is str:
+        img = cv2.imread(img)
+    
+    resized, pad = resize_and_pad(img, max_size)
+    resized = resized.astype(np.float32)
+    
+    if debug:
+        cv2.imwrite("intermediate.png", resized)
+
+    # Normalise!
+    resized /= 255.0
+    
+    return img, resized, pad
+
+
+
+
+
 if __name__ == "__main__":
  
     priROS = priROS()
     
     # Load the Edge TPU model
-    model_path = "path/to/your/model.tflite"
-    interpreter = edgetpu.make_interpreter(model_path)
+    interpreter = edgetpu.make_interpreter(opt.weights)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -83,6 +126,16 @@ if __name__ == "__main__":
             else:
                 # Resize image to match model input shape if needed
                 resized_image = cv2.resize(image, (input_details[0]['shape'][2], input_details[0]['shape'][1]))
+                
+                if interpreter is not None:
+                    input_size = common.input_size(interpreter)
+                    logger.debug("Expecting input shape: {}".format(input_size))
+                    input_size = input_size
+                else:
+                    logger.warn("Interpreter is not yet loaded")
+                    continue
+                
+                full_image, net_image, pad = get_image_tensor(image, input_size[0])
 
                 # Preprocess image for Edge TPU model
                 input_data = np.expand_dims(resized_image, axis=0)
