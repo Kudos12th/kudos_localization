0539d62892ca4c70f399de8f67e8293bc67cd511
YunHaaaa <qlxqlrt2012@gmail.com>
2024-02-14 15:40:06 +0900

    Feat: set filename

--- convert.py
+++ convert.py
@@ -39,22 +39,26 @@ else:
     print('Could not load weights from {:s}'.format(weights_filename))
     sys.exit(-1)
     
-# 모델을 추론 모드로 전환합니다
-model.eval()
+model.eval()  # 모델 전체를 평가 모드로 설정
 
+
+# 드롭아웃 레이어를 평가 모드로 설정
+for module in model.modules():
+    if isinstance(module, torch.nn.modules.Dropout):
+        module.train(False)
 
 # 모델에 대한 입력값
 x = torch.randn(batch_size, 3, 224, 224, requires_grad=True)
 torch_out = model(x)
 
 # 모델 변환
-torch.onnx.export(model,               # 실행될 모델
-                  x,                         # 모델 입력값 (튜플 또는 여러 입력값들도 가능)
-                  "super_resolution.onnx",   # 모델 저장 경로 (파일 또는 파일과 유사한 객체 모두 가능)
-                  export_params=True,        # 모델 파일 안에 학습된 모델 가중치를 저장할지의 여부
-                  opset_version=10,          # 모델을 변환할 때 사용할 ONNX 버전
-                  do_constant_folding=True,  # 최적화시 상수폴딩을 사용할지의 여부
-                  input_names = ['input'],   # 모델의 입력값을 가리키는 이름
-                  output_names = ['output'], # 모델의 출력값을 가리키는 이름
-                  dynamic_axes={'input' : {0 : 'batch_size'},    # 가변적인 길이를 가진 차원
-                                'output' : {0 : 'batch_size'}})
+torch.onnx.export(model,
+                  x,
+                  f"{weights_filename}.onnx",
+                  export_params=True,
+                  opset_version=10,
+                  do_constant_folding=True,
+                  input_names=['input'],
+                  output_names=['output'],
+                  dynamic_axes={'input': {0: 'batch_size'},
+                                'output': {0: 'batch_size'}})
